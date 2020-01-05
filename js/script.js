396b6deabeac12c1138bc049e8630b0ec4a36f2d
wiejak <wiejak555@gmail.com>
2020-01-05 15:39:25 +0100

    add active

--- js/script.js
+++ js/script.js
@@ -3,16 +3,18 @@
 function titleClickHandler() {
   console.log('Link was clicked!');
   console.log(event);
+  const clickedElement = this;
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.title a.active');
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
+  /* [DONE] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post');
 
   for (let activeArticle of activeArticles) {
