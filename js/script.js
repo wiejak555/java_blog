87f9a0cb978617b5b301ee9d337c3acb33f01f1a
wiejak <wiejak555@gmail.com>
2020-01-05 15:34:32 +0100

    remove active

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@ function titleClickHandler() {
   console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.title a.active');
 
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.post');
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
