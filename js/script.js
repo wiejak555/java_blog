2289e22e03801ed55f32f803cf94c4571115bf48
wiejak <wiejak555@gmail.com>
2020-01-05 15:49:34 +0100

    titleClickHandler ready

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@
 
 function titleClickHandler() {
   console.log('Link was clicked!');
-  console.log(event);
+
+  event.preventDefault();
   const clickedElement = this;
 
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.title a.active');
-
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
+
   /* [DONE] add class 'active' to the clicked link */
-  console.log('clickedElement:', clickedElement);
+
   clickedElement.classList.add('active');
+
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post');
 
@@ -21,11 +23,16 @@ function titleClickHandler() {
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targerArticle = document.querySelector(articleSelector);
+  console.log(targerArticle);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+  targerArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
